e6887e41234ede798f5955025dc06576e39964cb
Umut Köse <umut2819@gmail.com>
2023-01-22 01:07:41 +0300

    test messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 
 const Message = ({reciever}) => {
   return reciever?(
+    <div className="flex">
       <div className='flex justify-center ml-5 my-5 max-w-md  rounded-lg bg-white'>
         <span className="mr-2 p-2">
-          Hello dude what are you doing right know?
-          Hello dude what are you doing right know?
-          Hello dude what are you doing right know?
+          Same  message recieved...
         </span>
         <span className="flex flex-col justify-end pb-1 pr-2 text-xs opacity-60">
             19:20
         </span>
       </div>
+    </div>
   ):(
   <div className="flex flex-row-reverse">
     <div className='flex justify-center ml-5 my-5 max-w-md  rounded-lg bg-[#b3ff89]'>
     <span className="mr-2 p-2">
-      Hello dude what are you doing right know?
-      Hello dude what are you doing right know?
-      Hello dude what are you doing right know?
+    Same message sent... 
     </span>
     <span className="flex flex-col justify-end pb-1 pr-2 text-xs opacity-60">
-        19:20
+        01:06
     </span>
     </div>
   </div>
