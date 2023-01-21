e1e26c618a6a1e736b393923997017291ff9c31c
Umut Köse <umut2819@gmail.com>
2023-01-22 01:06:42 +0300

    Navbar bug fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar"
 import Search from "./Search"
 const Sidebar = () => {
   return (
-    <div className="sidebar flex-[2] border-r-[1px] border-[rgba(0,0,0,0.2)]">
+    <div className="sidebar  flex-[2] border-r-[1px] border-[rgba(0,0,0,0.2)]">
       <Navbar/>
       <Search/>
       <ChatList chatItems={[1,2,3,4,5,6,7,8,9]}/>
