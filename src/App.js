f9be1b9bd627596b034038b206c5241fa0f7af28
Umut Köse <umut2819@gmail.com>
2023-01-22 16:59:56 +0300

    container size changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import Home from "./pages/Home"
+import { useEffect } from "react";
 import "./App.css"
 function App() {
+  useEffect(()=>{
+    let vh=window.innerHeight*0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  })
   return (
     <div className="App">
       <Home/>
